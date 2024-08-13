3dd4a4710970fd88ce7f74ca86f6a67fe0b8c618
David Lee <david.lee@zuhlke.com>
2024-08-13 17:42:08 +0800

    Added necessary TIFFTags and xmp tag so that TIFFFILE recognized the generated file as BIF format

--- davidlee/src/mirax_bif_test.py
+++ davidlee/src/mirax_bif_test.py
@@ -1,3 +1,4 @@
+from numpy._core.multiarray import dtype
 import openslide
 import numpy as np
 import tifffile
@@ -25,15 +26,14 @@ def mrxs_to_bif(input_path, output_path = None, debug = False ):
 
         print(f"Number of levels: {slide.level_count}")
 
-        with tifffile.TiffWriter(output_path, bigtiff=True ) as tif:
+        # Need to set shaped to False so description is not set to the shape of the image 
+        with tifffile.TiffWriter(output_path, bigtiff=True, shaped=False ) as tif:
             base_level = 0
             levels_to_read = slide.level_count
 
 
             for level in range(base_level, (base_level + levels_to_read)):
                 print(f"\n=== Converting level {level} ===" )
-
-                extratags=[]
 
                 # Has to set subifds to None, and sub_file_type to None for the base level too.
                 sub_ifds = None
@@ -72,8 +72,31 @@ def mrxs_to_bif(input_path, output_path = None, debug = False ):
 
                 mdata = {}
 
-                print(f"metadata:")
-                print(mdata)
+                # IMPORTANT - xmp tag needs to be set
+                # 1. so that TIFFFILE can recognize it as bif format
+                # 2. so that it follows the BIF spec
+                #
+                # The root xml node must be <EncodedInfo>
+                xmp = r'''<?xml version='1.0' encoding='utf-8' ?>
+<EncodeInfo Ver="2">
+    <SlideInfo Rack="0" Slot="3" BaseName="00000000-ffe1-0000-0000-deadbeef0000" OutputFileName="/raid/HER2 IHC 2019-2 Core2 Ki67.bif">
+        <ServerDirectory Path="" Copy="false"/>
+        <LabelImage Path="" ShowLabel="1" LabelBoundary="0"/>
+        <iScan Mode="brightfield" Magnification="20" ScanRes="0.465"
+            UnitNumber="QIA00039" ScannerModel="VENTANA DP 600" Z-layers="1"
+            Z-spacing="1" UserName="Roche Service" BuildVersion="1.0.0.972-8f4bc6c"
+            BuildDate="2/18/2022 0:6:59 PM" SlideAnnotation="" ShowLabel="1"
+            LabelBoundary="0" Barcode1D="" Barcode2D="" FocusMode="0" FocusQuality="1"
+            ScanMode="1" ScanWhitePoint="235" Anonymization="0">
+        </iScan>
+    </SlideInfo>
+</EncodeInfo>'''
+
+                xmp_bytes = xmp.encode("utf-8")
+                extratags = [] if level > base_level else [
+                    (700, 'B', len(xmp_bytes), xmp_bytes, True)
+                ] 
+
 
                 tile_size = (256, 256)
                 format = "rgb"
@@ -85,6 +108,7 @@ def mrxs_to_bif(input_path, output_path = None, debug = False ):
 
                 print(f"creating numpy array")
                 data = np.array(image)
+                print(data.dtype)
 
                 print(f"writing to output file")
                 tif.write(
@@ -92,31 +116,82 @@ def mrxs_to_bif(input_path, output_path = None, debug = False ):
                     dtype=data.dtype,
                     tile=tile_size,
                     photometric=format,
+                    # IMPORTANT - ImageDescription (TIFFTag 270) needs to be in the format "level=<level> mag=<magnification> quality=<quality>".
+                    # For example: "leve=0 mag=40 quality=95".
+                    # TIFFFILE requires at least "level=" to be included.
+                    description=f"level={level}",
                     compression=compression,
                     resolution=resolution,
                     resolutionunit=resolution_unit,
                     subifds=sub_ifds,
                     subfiletype=sub_file_type,
-                    metadata=mdata,
+                    metadata=None,
                     extratags=extratags,
                     maxworkers=6,
+                    # software (TIFFTag 305) must start with "ScanOutputManager" otherwise TIFFFILE won't recognize it as bif file
+                    software="ScanOutputManager"
                 )
 
 
             # associated_images
+            label = slide.associated_images["label"]
+            label_data = np.array(label)
+            
+            # IMPORTANT - xmp tag should be set according to the bif spec. 
+            label_xmp = r'''<?xml version='1.0' encoding='utf-8' ?>
+<Metadata>
+    <iScan Mode="brightfield" Magnification="20" ScanRes="0.465"
+        UnitNumber="QIA00039" ScannerModel="VENTANA DP 600" Z-layers="1"
+        Z-spacing="1" UserName="Roche Service" BuildVersion="1.0.0.972-8f4bc6c"
+        BuildDate="2/18/2022 0:6:59 PM" SlideAnnotation="" ShowLabel="1"
+        LabelBoundary="0" Barcode1D="" Barcode2D="" FocusMode="0" FocusQuality="1"
+        ScanMode="1" ScanWhitePoint="235" Anonymization="0">
+        <AOI0 Left="206" Top="1888" Right="959" Bottom="1073"/>
+    </iScan>
+    <ProcessingParameters>
+        <Registration Method="None" UseLinearEncoder="1" Radius="0"
+            OverlapMinMicrons="" OverlapMaxMicrons="" ShiftMaxMicrons="" OverlapMin=""
+            OverlapMax="" ShiftMax=""/>
+        <Color TwistRGB="1,0,0,0,1,0,0,0,1" Applied="0"/>
+    </ProcessingParameters>
+</Metadata>'''
+
+            label_xmp_bytes = label_xmp.encode("utf-8")
+
+            tif.write(
+                label_data,
+                dtype=label_data.dtype,
+                # IMPORTANT - need to set ImageDescription (TIFFTag 270) to "Label_Image" according to the spec.
+                description="Label_Image",
+                extratags=[
+                    (700, 'B', len(label_xmp_bytes), label_xmp_bytes, True)
+                ],
+                # metadata doesn't seem to be necessary
+                metadata=None,
+                # software (TIFFTag 305) must start with "ScanOutputManager" otherwise TIFFFILE won't recognize it as bif file
+                software="ScanOutputManager",
+            )
+
             thumbnail = slide.associated_images["thumbnail"]
             thumbnail_data = np.array(thumbnail)
             tif.write(
                 thumbnail_data,
                 dtype=thumbnail_data.dtype,
-                metadata={
-                    "Name": "thumbnail"
-                }
+                # IMPORTANT - not sure if it's required by the spec but ImageDescription needs to be set to "Thumbnail" for a
+                # page to be recognized as thumbnail_by TIFFFILE
+                description="Thumbnail",
+                # metadata doesn't seem to be necessary
+                metadata=None,
+                # software (TIFFTag 305) must start with "ScanOutputManager" otherwise TIFFFILE won't recognize it as bif file
+                software="ScanOutputManager",
             )
+
         
         end_time = time.time()
         print("Successfully saved to " + output_path + " in " + str(end_time - start_time) + " seconds")
 
 
+
 source_path = "../samples/mrxs/CMU-1-Saved-1_16/CMU-1-Saved-1_16.mrxs"
+#foo(source_path)
 mrxs_to_bif(source_path)
