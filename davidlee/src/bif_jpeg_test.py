7c199a9c2b8264cf76fb4e30a4a1a6ab06f5e10c
David Lee <david.lee@zuhlke.com>
2024-08-16 18:45:07 +0800

    Added experimental code to convert all bif files in a dir to jpeg

--- davidlee/src/bif_jpeg_test.py
+++ davidlee/src/bif_jpeg_test.py
@@ -1,9 +1,13 @@
+from large_image.tilesource import base
 from tifffile import TiffFile
+import os
 from PIL import Image
 
 
-def to_jpeg(input_path, output_path= "bif-jpeg-outcome.jpg"):
+def to_jpeg(input_path):
+    output_path = os.path.splitext(input_path)[0] + ".jpg"
     print(f"Input: {input_path}")
+    print(f"Output: {output_path}")
 
     bif = TiffFile(input_path)
 
@@ -16,12 +20,23 @@ def to_jpeg(input_path, output_path= "bif-jpeg-outcome.jpg"):
             baseline = s
     
 
+    max_dimension = 65000
     print("baseline: ", baseline)
-    data = bif.pages[4].asarray() if baseline is None else baseline.asarray()
+    if baseline is None:
+        print(f"No baseline image.")
+        return
+    (width, height, depth) = baseline.shape
+    if width >= max_dimension or height > max_dimension:
+        print("Image dimension too big")
+        return
+
+    data = baseline.levels[0].asarray()
+    # data =  bif.pages[0].asarray() if baseline is None else baseline.asarray()
     # data = baseline.asarray()
 
     img = Image.fromarray(data)
     img.save(output_path)
+    print(f"Saved to {output_path}\n")
 
 
 
@@ -41,5 +56,12 @@ def dump_tiff_info(input_path):
         print(f"Number of pages = {len(pages_in_series)}")
 
 
-# dump_tiff_info("samples/bif/FBCa01-Ki67.bif")
-to_jpeg("samples/bif/FBCa01-Ki67.bif") 
+source_image = "../samples/bif/FBCa01-Ki67.bif" 
+# to_jpeg(source_image) 
+
+source_dir = "../samples/bif/"
+for file in os.listdir(source_dir):
+    if file.endswith(".bif"):
+        # print(os.path.join(source_dir, file))
+        to_jpeg(os.path.join(source_dir, file))
+        
